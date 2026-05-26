941d9cae033c96192e561f50e15b8a2079a07100
Tamim Chowdhury <tamim23544@iiitd.ac.in>
2026-05-26 17:03:23 +0600

    Fix Next.js basePath for GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/tamim-chowdhury-portfolio' : '',
   images: {
     unoptimized: true,
   },
